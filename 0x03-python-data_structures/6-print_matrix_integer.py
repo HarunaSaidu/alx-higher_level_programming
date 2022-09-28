93b0ad0dd397b6e514fbedbbec4bb56096f13a9c
HarunaSaidu <58739018+HarunaSaidu@users.noreply.github.com>
2022-09-28 02:14:34 +0100

    Create 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
-
-
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+    if not matrix:
+        return None
+    for inside in matrix:
+        if len(inside) == 0:
+            print()
+        for i in range(len(inside)):
+            print("{:d}".format(inside[i]),
+                  end="\n" if i is len(inside) - 1 else " ")
