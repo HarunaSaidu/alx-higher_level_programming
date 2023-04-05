53709ad3a617207f089af745005174f25233edc3
HarunaSaidu <58739018+HarunaSaidu@users.noreply.github.com>
2023-04-05 03:16:34 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 //  given integer
 const request = require('request');
 const [id] = process.argv.slice(2);
-request(`https://swapi-api.hbtn.io/api/films/${id}`, (err, res, body) => {
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, (err, res, body) => {
   if (err) console.error(err);
   console.log(JSON.parse(body).title);
 });
