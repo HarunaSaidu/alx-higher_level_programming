5cda01e633c0452bbac07f502d9af6c4ecfb1558
HarunaSaidu <58739018+HarunaSaidu@users.noreply.github.com>
2023-04-05 03:36:34 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 // prints the title of a Star Wars movie where the episode number matches a
 //  given integer
 const request = require('request');
-const [id] = process.argv.slice(2);
-request(`https://swapi-api.alx-tools.com/api/films/:${id}`, (err, res, body) => {
+const [id] = process.argv.slice(0);
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, (err, res, body) => {
   if (err) console.error(err);
   console.log(JSON.parse(body).title);
 });
